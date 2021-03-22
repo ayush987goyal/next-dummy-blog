9688ef6973d6c813160e2eb16efbaed7fabe354c
Ayush Goyal <goyalayu@amazon.com>
2021-03-22 15:35:43 +0530

    add custom rendered for markdown images

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import { Post } from '../../../models/post';
 import PostHeader from './post-header';
@@ -12,10 +13,42 @@ export interface PostContentProps {
 const PostContent: FC<PostContentProps> = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
